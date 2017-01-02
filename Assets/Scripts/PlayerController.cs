92787930e28ecf6411da57fd85a55e0f6aea7a4d
Nour_Ayad <nour6666@gmail.com>
2017-01-02 01:58:10 -0800

    Player
    Created the player game object along with a player controller, and added
    a test sprite. Player can now move around with the arrow keys, and has a
    set boundary in place. Also put in a test star field.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRigidBody;
+    public Boundary boundary;
+    public float speed;
+    
+    
+	// Use this for initialization
+	void Start ()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        playerRigidBody.velocity = movement * speed;
+        playerRigidBody.position = new Vector3(
+            Mathf.Clamp(playerRigidBody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(playerRigidBody.position.z, boundary.zMin, boundary.zMax)
+            );
+    }
+}
