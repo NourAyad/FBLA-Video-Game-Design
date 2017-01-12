b3c97bd710f6eb46a66078c9696a4cd4d272c427
Nour_Ayad <nour6666@gmail.com>
2017-01-12 00:28:17 -0800

    Asteroid
    Created asteroid prefab which can destroy the player and be destroyed by
    shots.

---
+++ Assets/DestroyByContact.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+    {
+       // Debug.Log(gameObject.transform.position);
+        Debug.Log(rb.velocity);
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
